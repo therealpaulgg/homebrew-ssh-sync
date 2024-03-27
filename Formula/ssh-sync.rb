e7c7d6054b799be217209b64feb83270040c3be9
GitHub Action <action@github.com>
2024-03-27 06:38:49 +0000

    Update formula to version v0.0

--- Formula/ssh-sync.rb
+++ Formula/ssh-sync.rb
@@ -1,8 +1,8 @@
 class SshSync < Formula
     desc "SSH syncing application built with Go"
     homepage "https://therealpaulgg.github.io/ssh-sync"
-    url "https://github.com/therealpaulgg/ssh-sync/archive/refs/tags/v0.3.6.tar.gz"
-    sha256 "1febbce02a1ae46cc35d347da0f2bed798b0a743805438cc083c4023ab0d143e"
+    url "https://api.github.com/repos/therealpaulgg/ssh-sync/tarball/v0.3.8"
+    sha256 "6365963ce8e3ab617daa61bce4e21d7a58c15d0c892c99ccbb1720f714566842"
     license "MIT"
   
     depends_on "go" => :build
